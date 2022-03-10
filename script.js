e7b7dd0309d8b8f21f990a340e269acedd62e090
keyp0s <aidentubman@gmail.com>
2022-03-10 19:02:20 +1100

    working auto

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 //declaring variables
-var base = []
-var target = []
+var base = [];
+var target = [];
 
 //gets target and base language lists
-document.querySelectorAll("div.baseLanguage").forEach(i => base.push(i.innerText));
-document.querySelectorAll("div.targetLanguage").forEach(i => target.push(i.innerText));
-
-//function to input text and submit
-async function submit(answer) {
-    (await document.querySelector("button#explanation-button")).click();
-    document.querySelector("input#answer-text").value = answer;
+function collect(){
+    document.querySelectorAll("div.baseLanguage").forEach(i => base.push(i.innerText));
+    document.querySelectorAll("div.targetLanguage").forEach(i => target.push(i.innerText));
 }
 
-//gets answer
-question = document.getElementById("question-text").innerText;
-answer = (target[base.indexOf(question)])
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-submit(answer);
+//function to input text and submit
+async function start(time){
+    while (true){
+        async function submit(answer) {
+            (await document.querySelector("button#explanation-button")).click();
+            document.querySelector("input#answer-text").value = answer;
+        }
+        
+        //gets answer
+        question = document.getElementById("question-text").innerText;
+        answer = (target[base.indexOf(question)]);
+        
+        submit(answer);
+        await sleep(time);
+    }
+}
